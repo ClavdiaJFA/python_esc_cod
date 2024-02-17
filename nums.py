8e11e9257a91c06ad65e46eba06cd345963f539e
ClaudiaBlumen <73711809+ClavdiaJFA@users.noreply.github.com>
2024-02-17 17:45:50 -0600

    Update nums.py

--- nums.py
+++ nums.py
@@ -17,3 +17,30 @@ for num in nums:
 # 2 Imprimir si cada número en la lista es par o impar
 cubos_mayor_5000 = [num for num in nums if num**3 > 5000]
 print("Números cuyo cubo es mayor a 5000:", cubos_mayor_5000)
+
+
+#Resultados:
+#Cuadrados de los números: [4, 25, 225, 441, 256, 324, 49, 324, 1369, 1089, 6084, 6724, 784, 625, 144, 1600, 9, 
+#400, 2025, 3025, 400, 441]
+#2 es par
+#5 es impar
+#15 es impar
+#21 es impar
+#16 es par
+#18 es par
+#7 es impar
+#18 es par
+#37 es impar
+#33 es impar
+#78 es par
+#82 es par
+#28 es par
+#25 es impar
+#40 es par
+#3 es impar
+#20 es par
+#45 es impar
+#55 es impar
+#20 es par
+#21 es impar
+#Números cuyo cubo es mayor a 5000: [21, 18, 18, 37, 33, 78, 82, 28, 25, 40, 20, 45, 55, 20, 21]
